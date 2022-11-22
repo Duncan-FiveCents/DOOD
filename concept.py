1e34461c866b720905d5b679807167084caf60f6
Duncan Nickel <d.nickel1@share.epsb.ca>
2022-11-22 12:19:09 -0700

    window stufffff

--- concept.py
+++ concept.py
@@ -5,3 +5,22 @@ title: proof of concept/testing for rendering
 author: Duncan Nickel
 date-created: 22/11/2022
 '''
+
+import pygame
+import pygame.gfxdraw
+
+from window import Window
+
+if __name__ == "__main__":
+    pygame.init()
+    WINDOW = Window()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+
+        WINDOW.screen.fill(WINDOW.bg)
+        WINDOW.frame.tick(60)
+        pygame.display.flip()
