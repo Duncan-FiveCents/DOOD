30b115efb42ab3d61f164176d6f5a9cf89d6cf71
Duncan Nickel <d.nickel1@share.epsb.ca>
2022-11-29 11:38:34 -0700

    words on ui

--- userInterface.py
+++ userInterface.py
@@ -13,14 +13,19 @@ class HUD:
         pygame.font.init()
         self.window = SCREEN
         self.surface = self.window.screen
-        self.font = pygame.font.SysFont("UI\Pixel Combat.otf",30)
+        self.font = "UI\Pixel Combat.otf"
 
     def mainHud(self,WEAPONS,ACTIVE_WEAPON,HEALTH,SHIELD,FPSCOUNTER):
+        # Weapon
         GUN = pygame.image.load("UI\Gun placeholder.png").convert()
         GUN = pygame.transform.scale2x(GUN)
         self.surface.blit(GUN,(0,0))
 
+        # Base Hud
         HUD = pygame.image.load("UI\DOOD UI1.png").convert()
         self.surface.blit(HUD,(0,0))
 
-        if FPSCOUNTER: self.surface.blit(self.font.render(str(int(self.window.frame.get_fps())),False,(0,0,0)),(0,0)) 
+        self.surface.blit(pygame.font.SysFont(self.font,28).render("WILLPOWER",False,(255,0,0)),(139,450))
+        self.surface.blit(pygame.font.SysFont(self.font,28).render("SHEILD",False,(255,0,0)),(405,450))
+
+        if FPSCOUNTER: self.surface.blit(pygame.font.SysFont(self.font,30).render(str(int(self.window.frame.get_fps())),False,(0,0,0)),(616,364))
